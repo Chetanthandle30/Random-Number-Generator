114ba03f70d37b01fe9cd07ca1fa00f18082a62e
Chetan Thandle <87128154+Chetanthandle30@users.noreply.github.com>
2024-04-03 04:55:37 +0530

    Update index.js
    New commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//this is a comment
+//Random Number Generator   
+
+const myButton = document.getElementById("buttonID");
+const myLabel = document.getElementById("myLabel")
+
+const max = 6;
+const min=1;
+let randomNumber;
+
+myButton.onclick = function(){
+    randomNumber = Math.floor(Math.random()*max)+min ;
+    myLabel.textContent = randomNumber
+}
+
